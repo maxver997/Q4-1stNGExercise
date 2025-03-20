e068eb4a5a408b01b28df1ba2ae1c7bd3a01acd8
maxver997 <168631711+maxver997@users.noreply.github.com>
2025-03-20 08:52:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/draw", (req, res) => {
+  const sideLength = req.query.sideLength;
+  res.render("draw", {sideLength});
+});
+app.post("/draw", (req, res) => {
+  const sideLength = req.body.sideLength;
+  res.render("draw", {sideLength});
+});
+
+
+
 // Create express route binder for draw.hbs and get the data from the url as parameters
 // that came from index.hbs
 
